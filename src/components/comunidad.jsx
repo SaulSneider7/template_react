2ad91a2bc1e204bb9b449436ad5fa12e209f0ba9
RolandoMamani <rolomc2016@gmail.com>
2025-11-12 10:42:41 -0500

    edit complete

--- src/components/comunidad.jsx
+++ src/components/comunidad.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged, updateProfile } from "firebase/auth";
 
 function Comunidad() {
     const [nombre, setNombre] = useState("");
@@ -7,15 +7,23 @@ function Comunidad() {
     const [foto, setFoto] = useState("");
     const [cargando, setCargando] = useState(true);
 
+    const [nuevoNombre, setNuevoNombre] = useState("");
+    const [nuevaFoto, setNuevaFoto] = useState("");
+
+    const auth = getAuth();
+
     useEffect(()=>{
-        const auth = getAuth();
+        
         const user = auth.currentUser;
         // Escucha si el usuario inicia o cierra sesión
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
                 setEmail(user.email);
                 setNombre(user.displayName || "Usuario sin nombre");
-                setFoto(user.photoURL)
+                setFoto(user.photoURL || "user.webp");
+
+                setNuevoNombre(user.displayName || "");
+                setNuevaFoto(user.photoURL || "");
             } else {
                 setEmail("");
                 setNombre("");
@@ -27,16 +35,47 @@ function Comunidad() {
         return () => unsubscribe();
     }, []);
 
+    //Funcion para editar mis datos
+    const actualizarPerfil = async() => {
+        const user = auth.currentUser;
+        if (!user) return alert("No hay un usuario activo");
+        await updateProfile(user,{
+            displayName: nuevoNombre || user.displayName,
+            photoURL: nuevaFoto || user.photoURL
+        })
+        alert("Perfil actualizado");
+        setNombre(nuevoNombre);
+        setFoto(nuevaFoto);
+    }
+
+
     if (cargando) {
         return <p className="text-center mt-10 text-gray-500">Cargando datos...</p>;
     }
     
     return (
         <div className="p-8 text-center">
-            <img src={foto} alt="" />
+            <img className="w-20 h-20" src={foto} alt="" />
             <h1 className="text-3xl font-bold mb-4">Bienvenido a la comunidad</h1>
             <p className="text-lg">Hola, <strong>{nombre}</strong></p>
             <p className="text-gray-600">Tu correo: {email}</p>
+
+            <hr className="my-6" />
+
+            <h2>Formulario para editar mis datos</h2>
+            <input 
+                type="text" 
+                placeholder="actualizar mi nombre" 
+                value={nuevoNombre}
+                onChange={(e) => setNuevoNombre(e.target.value)}    
+            />
+            <input 
+                type="text" 
+                placeholder="actualizar mi foto" 
+                value={nuevaFoto}
+                onChange={(e) => setNuevaFoto(e.target.value)}
+            />
+            <button onClick={actualizarPerfil}>Actualizar perfil</button>
 
             <hr className="my-6" />
             <p className="text-gray-700">
